e0d6ec5648c87280fa9ea0de562137b5c53b01df
Longkai Cao <lkcao@thoughtworks.com>
2012-12-09 09:06:41 +0800

    delete unused classes

--- src/main/java/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-
-    public String sayHelloTo(String name) {
-        return "hello world longkai!";  //To change body of created methods use File | Settings | File Templates.
-    }
-}
